26d35b70745bfab0e2face9ee8dd04c2b6e5849f
Sam Martinez & Kelli Maragret <student@epicodus.com>
2015-08-26 15:54:55 -0700

    add twig files

--- app/app.php
+++ app/app.php
@@ -5,6 +5,8 @@
     require_once __DIR__."/../src/Author.php";
 
     $app = new Silex\Application();
+    // $app [debug] = true;
+
     $server = 'mysql:host=localhost;dbname=library';
     $username = 'root';
     $password = 'root';
@@ -15,12 +17,25 @@
         'twig.path' => __DIR__."/../views"
     ));
 
+
+
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
     //Get Calls
     $app->get("/", function() use ($app) {
+
         return $app['twig']->render("index.html.twig", array('books' => Book::getAll(), 'authors' => Author::getAll()));
+    });
+
+    //Post Calls
+    $app->post("/books", function() use ($app) {
+        $title = $_POST['title'];
+        $genre = $_POST['genre'];
+        $book = new Book($title, $genre);
+        $book->save();
+
+        return $app['twig']->render("books.html.twig", array('books' => Book::getAll()));
     });
 
     return $app;
