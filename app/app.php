87660adefdf9bb50b711f8904bdf0d2e7dacf7b9
marcos moncivais & Phillip Shannon <student@epicodus.com>
2015-08-27 14:49:14 -0700

    add routes to books and authors in app.php

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Book.php";
     require_once __DIR__."/../src/Author.php";
+    require_once __DIR__."/../src/Patron.php";
 
     $app = new Silex\Application();
     // $app [debug] = true;
@@ -19,35 +20,23 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-    //Get Calls
+    //Main page
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render("index.html.twig", array('books' => Book::getAll(), 'authors' => Author::getAll()));
+        return $app['twig']->render("index.html.twig");
     });
 
-    $app->get("/book_info/{id}", function($id) use ($app) {
+    //Librarian Portal
+    $app->get("/librarian", function() use ($app) {
+        return $app['twig']->render("librarian.html.twig");
+    });
+
+    $app->get("/search_books", function() use($app) {
+        return $app['twig']->render("librarian_books.html.twig", array('books' => Book::getAll()));
+    });
+    $app->get("/book_info/{id}", function($id) use($app) {
         $book = Book::find($id);
-        return $app['twig']->render("book_info.html.twig", array('book' => $book));
-    });
-
-    //Post Calls
-    $app->post("/books", function() use ($app) {
-        $title = $_POST['title'];
-        $genre = $_POST['genre'];
-        $book = new Book($title, $genre);
-        $book->save();
-
-        return $app['twig']->render("books.html.twig", array('books' => Book::getAll()));
-    });
-
-    $app->post("/book_info/{id}", function($id) use ($app) {
-        $name = $_POST['name'];
-        $author = new Author($name);
-        $author->save();
-
-        $book = Book::find($id);
-
-        return $app['twig']->render("book_info.html.twig", array('book' => $book, 'authors' => Author::getAll()));
+        return $app['twig']->render('librarian_book.html.twig', array('book' => $book, 'authors' => $book->getAuthors(), 'all_authors' => Author::getAll()));
     });
 
 
